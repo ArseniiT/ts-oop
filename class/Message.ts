25758dd5e71497b3deed6747b682292b3d20bb5b
arseniit <arseniit@gmail.com>
2023-12-12 13:58:57 +0100

    Add Get message status

--- class/Message.ts
+++ class/Message.ts
@@ -9,12 +9,22 @@ export class Message {
         this.isSent =false;
     }
 
+    get messageStatus(): string {
+        const sentMessage = this.isSent ? 'Has been sent.' : 'Has not been sent.';
+        return `${this.message} | ${sentMessage}`;
+    }
+
     previewMessage(): string {
         return this.message.slice(0, 15).concat('...');
     }
 }
 
-const message2 = new Message(
+const message = new Message(
     'Unlock Your Potential with TypeScript Mastery',
     'Master TypeScript: Dive into Object-Oriented Programming (OOP)!'
 );
+
+// test
+
+message.messageStatus;
+message.previewMessage();
