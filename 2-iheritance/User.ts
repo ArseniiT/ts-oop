19ffd7cc3912053b48d849b180af22041427b22d
arseniit <arseniit@gmail.com>
2023-12-12 18:53:41 +0100

    Realize generics in the place of any

--- 2-iheritance/User.ts
+++ 2-iheritance/User.ts
@@ -1,10 +1,10 @@
-export class User {
+export class User<T> {
     name: string;
     age: number;
     email: string;
     isMale: boolean;
 
-    public classicUserData: any;
+    public classicUserData: T;
 
     public mergeClassicUser(params: any): void {
         const { name, isMale, age, email } = this;
@@ -21,9 +21,11 @@ interface ClassicUser2 {
     name: { first: string, middle: string, last: string };
 }
 
-const user1 = new User();
+const user1 = new User<ClassicUser>();
 user1.mergeClassicUser({name: { first: 'Dylan', last: 'Israel'}});
+user1.classicUserData.name.first;
 
-const user2 = new User();
+const user2 = new User<ClassicUser2>();
 user1.mergeClassicUser({name: { first: 'Dylan', last: 'Israel', middle: 'McCane'}});
+user2.classicUserData.name.middle;
 
