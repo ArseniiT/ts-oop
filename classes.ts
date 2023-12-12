183b88e4d1ce22b840bf2c35dac0cea4ce6da5f7
arseniit <arseniit@gmail.com>
2023-12-12 12:37:26 +0100

    add id to the classical var of messages

--- classes.ts
+++ classes.ts
@@ -1,3 +1,3 @@
-const message1 = {title: undefined, message: undefined};
-const message2 = {title: undefined, message: undefined};
-const message3 = {title: undefined, message: undefined};
+const message1 = {title: undefined, message: undefined, id: undefined};
+const message2 = {title: undefined, message: undefined, id: undefined};
+const message3 = {title: undefined, message: undefined, id: undefined};
