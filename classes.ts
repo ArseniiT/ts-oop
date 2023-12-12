bfcede17f889a146cfd19d6d77bded6f5cf860f5
arseniit <arseniit@gmail.com>
2023-12-12 12:39:48 +0100

    add Message class as a base for future messages

--- classes.ts
+++ classes.ts
@@ -1,3 +1,12 @@
 const message1 = {title: undefined, message: undefined, id: undefined};
 const message2 = {title: undefined, message: undefined, id: undefined};
 const message3 = {title: undefined, message: undefined, id: undefined};
+
+export class Message {
+    title;
+    message;
+    id;
+}
+
+const messag4 = new Message()
+const messag5 = new Message()
